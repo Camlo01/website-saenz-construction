0267611b8a09f4e1b0f107218860730a824cfc2e
unknown <camilobeltran174@gmail.com>
2023-06-11 20:56:01 -0500

    Add content to Schedule section

--- src/sections/schedule/ScheduleSection.jsx
+++ src/sections/schedule/ScheduleSection.jsx
@@ -4,6 +4,15 @@ export default function Schedule() {
     return (
         <>
             <div className="section-schedule">
+                <div className="section-schedule__layer">
+                    <div className="section-schedule__body">
+                        <div className="section-schedule__text">
+                            <h2>SCHEDULE YOUR FREE CONSULTATION</h2>
+                            <p>For a durable and beautiful roof guaranteed to stand the test of time, meet with your local roofing experts.</p>
+                        </div>
+                        <button><a href="#">REQUEST A QUOTE</a></button>
+                    </div>
+                </div>
             </div>
         </>
     )
