9a92ddf04f5c55eae1a9001e42a50080310c990e
unknown <camilobeltran174@gmail.com>
2023-06-17 20:06:21 -0500

    Add 2 cards for new images

--- src/sections/portfolio/PortfolioSection.jsx
+++ src/sections/portfolio/PortfolioSection.jsx
@@ -22,9 +22,17 @@ export default function Portfolio() {
                                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
                                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
                                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
                             </div>
                             <div className="carousel-inner">
                                 <div className="carousel-item active">
+                                    <img src="images/roof-gallery-1.jpg" className="d-block w-85" alt="..." />
+                                </div>
+                                <div className="carousel-item">
+                                    <img src="images/roof-gallery-1.jpg" className="d-block w-85" alt="..." />
+                                </div>
+                                <div className="carousel-item">
                                     <img src="images/roof-gallery-1.jpg" className="d-block w-85" alt="..." />
                                 </div>
                                 <div className="carousel-item">
