858ff54de50ff9df41e0db05ea381df096eb352a
Camilo <camilobeltran@MacBook-Pro-Milo.local>
2023-07-03 17:02:13 -0500

    Add Gallery component to the '/gallery' route

---
+++ src/pages/Gallery.jsx
@@ -0,0 +1,15 @@
+import Footer from "../sections/footer/FooterSection"
+
+/**
+ * 
+ * @returns Component of Gallery
+ */
+export default function Gallery() {
+
+    return (
+        <>
+            <h1>Gallery Page</h1>
+            <Footer />
+        </>
+    )
+}
