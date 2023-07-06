bd9c344c85f941bfe5d1137d6d7592e33d69ae06
Camilo <camilobeltran@MacBook-Pro-Milo.local>
2023-07-05 20:53:00 -0500

    indicate new location of Gallery.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Gallery from './pages/Gallery.jsx';
+import Gallery from './pages/Gallery/Gallery.jsx';
 
 const router = createBrowserRouter([
   {
