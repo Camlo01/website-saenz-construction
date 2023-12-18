7a953b791dab9b2f5bc6f2e0be75616c6f201bf2
Camilo <camilobeltran174@gmail.com>
2023-12-18 12:09:01 -0500

    Add logic for contact form

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -13,24 +13,29 @@ export default function Header() {
     const [description, setDescription] = useState('')
     const [zip, setZip] = useState('')
 
+    const [disabledForm, setDisabledForm] = useState(false)
+
     useEffect(() => {
-      
-        // to fix problem when the website load
-        setTimeout(() => {
-            navbarColor()
-            navbarOpenMenu()
-            console.log("Message in console")
-        }, 250);
-
+        navbarColor()
+        navbarOpenMenu()
     }, null)
-    
 
-    // const handleGetFreeQuote = () => {
+    const handleContactForm = (e) => {
+        e.preventDefault();
 
-    //     const data = { fullName, email, phone, address, source, description, zip }
-    //     // sendEmail(data); // Method that should go in other class with the logic of send emails
+        const data = {
+            fullName: fullName,
+            email: email,
+            phone: phone,
+            address: address,
+            source: source,
+            description: description,
+            zip: zip
+        }
 
-    // }
+        sendEmail({ data, setDisabledForm});
+
+    }
 
     /**
      * Function to improve the nav items behavior
@@ -130,28 +135,26 @@ export default function Header() {
                             </div>
                         </div>
 
-                        <form action='https://formsubmit.co/a5ffb29a18338d761ffae78c515265c5' method='POST' className='header__form' id='get-free-quote'>
+                        <form onSubmit={handleContactForm} className='header__form' id='get-free-quote'>
 
                             <h2>Get Started Now</h2>
 
-                            <input name='Full Name' onChange={e => { setFullName(e.target.value) }} placeholder='Full Name' type='text' />
-                            <input name='Email Address' onChange={e => { setEmail(e.target.value) }} placeholder='Email Address' type='email'/>
-                            <input name='Phone Number' onChange={e => { setPhone(e.target.value) }} placeholder='Phone Number' type='text'/>
-                            <input name='Full Address' onChange={e => { setAddress(e.target.value) }} placeholder='Full Address' type='text' />
-                            <select name="From Where heard about us" id="source-client" onChange={e => { setSource(e.target.value) }} >
-                                <option value="none" selected disabled>How did you hear about us?</option>
+                            <input disabled={disabledForm} value={fullName} name='Full Name' onChange={e => { setFullName(e.target.value) }} placeholder='Full Name' type='text' />
+                            <input disabled={disabledForm} value={email} name='Email Address' onChange={e => { setEmail(e.target.value) }} placeholder='Email Address' type='email' />
+                            <input disabled={disabledForm} value={phone} name='Phone Number' onChange={e => { setPhone(e.target.value) }} placeholder='Phone Number' type='text' />
+                            <input disabled={disabledForm} value={address} name='Full Address' onChange={e => { setAddress(e.target.value) }} placeholder='Full Address' type='text' />
+                            <select disabled={disabledForm} name="From Where heard about us" id="source-client" onChange={e => { setSource(e.target.value) }} >
+                                <option value="none" selected disabled >How did you hear about us?</option>
                                 <option value="Internet Search">Internet Search</option>
                                 <option value="Referral">Referral</option>
                                 <option value="Saenz Car">Saenz Car</option>
                                 <option value="Flyer">Flyer</option>
                                 <option value="Facebook">Facebook</option>
                             </select>
-                            <textarea name='Proyect description' onChange={e => { setDescription(e.target.value) }} placeholder='Proyect Description' cols="30" rows="10"></textarea>
-                            <input name='ZIP code' onChange={e => { setZip(e.target.value) }} placeholder='ZIP Code' type="text" />
+                            <textarea disabled={disabledForm} value={description} name='Proyect description' onChange={e => { setDescription(e.target.value) }} placeholder='Proyect Description' cols="30" rows="10"></textarea>
+                            <input disabled={disabledForm} value={zip} name='ZIP code' onChange={e => { setZip(e.target.value) }} placeholder='ZIP Code' type="text" />
 
-                            <button type="submit" value='enviar' >GET FREE QUOTE</button>
-                            <input type="hidden" name='_next' value='https://saenz-construction.com' />
-                            <input type="hidden" name='_captcha' value='false' />
+                            <button disabled={disabledForm} type="submit" value='enviar' >GET FREE QUOTE</button>
 
                         </form>
                     </div>
@@ -189,4 +192,82 @@ export default function Header() {
         </>
     )
 
+}
+
+/**
+ * Method to send emails by calling php script file and return status code
+ * @param {*} props 
+ */
+async function sendEmail(props) {
+
+    const data = props.data;
+
+    if (isValidForm(data)) {
+        try {
+
+            const response = await fetch('https://saenz-construction.com/server/sendMail.php', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+
+            const status = response.status;
+            console.log(status)
+
+            if (status == 200){
+                alert("The message was sent successfully")
+                props.setDisabledForm(true)
+            }
+
+
+        } catch (e) {
+            console.log("Something Went Wrong!")
+            props?.setResponse(500)
+        }
+    } else {
+        props?.setResponse(400)
+    }
+}
+
+function isValidForm(data) {
+
+    if (isAnyEmptyField(data)) {
+        alert('Please fill in all the fields.')
+        return false
+    }
+
+    if (!isEmailValid(data.email)) {
+        alert("Enter a valid email")
+        return false
+    }
+
+    if (data.source === '' || data.source === 'none') {
+        alert('Please select how did you hear about us')
+        return false
+    }
+
+    if (descriptionExceedLimit(data.description)) {
+        alert("Theres a limit of 350 characters")
+        return false
+    }
+    return true
+}
+
+function isEmailValid(email) {
+    return /^(?=.{1,50}$)[A-Za-z0-9._%+-]+@.*/.test(email)
+}
+
+function isAnyEmptyField(data) {
+    return (data.fullName === "" ||
+        data.email === "" ||
+        data.phone === "" ||
+        data.address === "" ||
+        data.description === "" ||
+        data.zip === "")
+}
+
+function descriptionExceedLimit(text) {
+    return (text.length > 350)
 }
